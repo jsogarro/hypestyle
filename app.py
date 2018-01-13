04c367be80d1130888a593488fa7e9c979fec1f2
Jamal O'Garro <jamal.ogarro[at]gmail.com>
2018-01-13 12:37:08 -0500

    refactor endpoint boilerplates to use Flask RESTful

--- app.py
+++ app.py
@@ -1,33 +1,38 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, request, render_template
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
+
 
 # GET /
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# GET /brands
-@app.route('/brands')
-def get_brands():
-    return jsonify({'success': True})
+class BrandsIndex(Resource):
+    def get(self):
+        return {'success': True}
 
-# GET /brands/:id
-@app.route('/brands/<string:id>')
-def get_brand(id):
-    return jsonify({'success': True})
+class Brand(Resource):
+    def get(self, id):
+        print(id)
+        return {'success': True, 'request': id}
 
-# POST /brands
-@app.route('/brands', methods=['POST'])
-def create_brand():
-    req = request.get_json()
-    return jsonify({'success': True, 'brand': req})
+    def post(self, id):
+        request_json = request.get_json()
+        print(request_json)
+        return {'success': True, 'request': request_json}
 
-# POST /brands/:id/item
-@app.route('/brands/<string:id>/item', methods=['POST'])
-def get_item_in_brand(id):
-    return jsonify({'success': True})
+class BrandItem(Resource):
+    def post(self, id):
+        request_json = request.get_json()
+        print(request_json)
+        return {'success': True, 'request': request_json, 'id': id}
 
+api.add_resource(BrandsIndex, '/brands')
+api.add_resource(Brand, '/brands/<string:id>')
+api.add_resource(BrandItem, '/brands/<string:id>/item')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
